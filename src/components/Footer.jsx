fcd2d5ff5d12a32e1672671efcbf9fe064df8126
Dinesh <dineshlegha23@gmail.com>
2024-04-09 16:43:28 +0530

    completed footer responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div className="bg-black/80" id="contact">
       <div className="max-w-[1200px] mx-auto p-5 text-white  mt-10 pt-16">
-        <div className="grid grid-cols-4 md:grid-cols-3 pb-10 border-b-[1px] border-gray-400">
-          <div className="max-w-[400px] col-span-2 xs:col-span-4">
+        <div className="grid grid-cols-4 md:grid-cols-3 xs:grid-cols-1 pb-10 border-b-[1px] border-gray-400">
+          <div className="max-w-[400px] col-span-2 xs:col-span-1">
             <div className="mb-5">
               <img src={assets.logo} alt="tomato logo" />
             </div>
@@ -34,7 +34,7 @@ const Footer = () => {
               />
             </div>
           </div>
-          <div className="justify-self-end md:justify-self-center xs:mt-10 xxs:col-span-4">
+          <div className="xs:mt-10 justify-self-end md:justify-self-start">
             <p className="text-xl font-semibold mb-5">COMPANY</p>
             <ul className="text-gray-300">
               <li className="cursor-pointer hover:text-white">Home</li>
@@ -45,7 +45,7 @@ const Footer = () => {
               </li>
             </ul>
           </div>
-          <div className="justify-self-end md:justify-self-start mt-10 xs:ml-10">
+          <div className="justify-self-end md:justify-self-start xs:mt-10 md:mt-10">
             <p className="text-xl font-semibold mb-5">GET IN TOUCH</p>
             <ul className="text-gray-300">
               <li className="cursor-pointer hover:text-white">
